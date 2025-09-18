4c69f002a43d9c6abc4adef832cbcb1ceb3a32f6
Victor-Gabriel Costin <costinv92@gmail.com>
2025-09-18 17:23:26 +0300

    examples: add connection management examples
    Signed-off-by: Victor-Gabriel Costin <costinv92@gmail.com>

--- examples/fastsignal_examples.cpp
+++ examples/fastsignal_examples.cpp
@@ -112,6 +112,92 @@ void virtual_member_function_example()
     delete object;
 }
 
+struct Subject
+{
+    fastsignal::FastSignal<void()> sig;
+
+    void subject_changed() { sig(); }
+};
+
+struct ObserverManual
+{
+    int id;
+    fastsignal::ConnectionView conn;
+
+    ObserverManual(int id) : id(id) {}
+    ~ObserverManual() { conn.disconnect(); }
+
+    void connect(Subject& subject) { conn = subject.sig.add<&ObserverManual::on_subject_changed>(this); }
+    void on_subject_changed() { std::cout << "ObserverManual(" << id << "): on_subject_changed()\n"; }
+};
+
+// ObserverAutomatic inherits from fastsignal::Disconnectable
+struct ObserverAutomatic : public fastsignal::Disconnectable
+{
+    int id;
+    // ObserverAutomatic doesn't have a destructor
+    ObserverAutomatic(int id) : id(id) {}
+
+    // The ConnectionView returned by add() is not stored anywhere
+    void connect(Subject& subject) { subject.sig.add<&ObserverAutomatic::on_subject_changed>(this); }
+    void on_subject_changed() { std::cout << "ObserverAutomatic(" << id << "): on_subject_changed()\n"; }
+};
+
+void observer_manual_management_example()
+{
+    std::cout << "Observer manual management example:\n";
+    std::cout << "====================================\n";
+
+    Subject subject;
+    {
+        ObserverManual observer1(1);
+        ObserverManual observer2(2);
+
+        observer1.connect(subject);
+        observer2.connect(subject);
+
+        std::cout << "Subject changed\n";
+        // Will print:
+        // Observer(1): on_subject_changed()
+        // Observer(2): on_subject_changed()
+        subject.subject_changed();
+    }
+    // ObserverManual objects are destroyed here
+    // They are disconnected from the signal by the destructor
+    std::cout << "ObserverManual objects are destroyed here\n";
+    std::cout << "Subject changed\n";
+    // Will print nothing
+    subject.subject_changed();
+    std::cout << "\n";
+}
+
+void observer_automatic_management_example()
+{
+    std::cout << "Observer automatic management example:\n";
+    std::cout << "====================================\n";
+    Subject subject;
+    {
+        ObserverAutomatic observer1(1);
+        ObserverAutomatic observer2(2);
+
+        observer1.connect(subject);
+        observer2.connect(subject);
+
+        std::cout << "Subject changed\n";
+        // Will print:
+        // ObserverAutomatic(1): on_subject_changed()
+        // ObserverAutomatic(2): on_subject_changed()
+        subject.subject_changed();
+    }
+    // ObserverAutomatic objects are destroyed here
+    // They are disconnected from the signal automatically
+    std::cout << "ObserverAutomatic objects are destroyed here\n";
+    std::cout << "Subject changed\n";
+    // Will print nothing
+    subject.subject_changed();
+    std::cout << "\n";
+}
+
 int main()
 {
     free_function_no_param_example();
@@ -119,4 +205,7 @@ int main()
     free_function_with_complex_param_example();
     member_function_with_complex_param_example();
     virtual_member_function_example();
+
+    observer_manual_management_example();
+    observer_automatic_management_example();
 }
